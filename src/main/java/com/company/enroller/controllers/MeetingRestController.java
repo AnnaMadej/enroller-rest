a96c331abf4abb2b11c4d3e173bf84db760d14fc
Anna Madej <aniamadej123@gmail.com>
2020-04-19 17:33:32 +0200

    added requestMapping for /meetings/meetingId

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,15 @@ public class MeetingRestController {
 	public ResponseEntity<?> getAllMeetings() {
 		Collection<Meeting> meetings = meetingService.getAll();
 		return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET )
+	public ResponseEntity<?> getMeeting(@PathVariable("id") long meetingId) {
+		Meeting meeting = meetingService.findById(meetingId);
+		
+		if(meeting == null) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity(meeting, HttpStatus.OK);
 	}
 }
